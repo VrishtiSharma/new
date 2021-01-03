a3b7e07d7d0ead82a2316f6dbb6558ba232e813f
i_Harshit_sharma <harshit2neeraj@gmail.com>
2021-01-03 22:52:24 +0530

    initial commit

---
+++ starl.cpp
@@ -0,0 +1,22 @@
+#include<iostream.h>
+#include<conio.h>
+void main ()
+{
+clrscr();
+int n;
+cout<<"Enter number of rows";
+cin>>n;
+int k=2*n-2;
+for(int i=0;i<n;i++)
+ {
+   for(int j=0;j<k;j++)
+    cout<<"  ";
+    k=k-1;
+    for( j=0;j<=i;j++)
+    {
+     cout<<"* ";
+     }
+     cout<<endl;
+     }
+getch();
+}
